125ec8c269ca4c130b8f0624b3d756f03ad3c8ce
Kell Lit <107881418+kellie-lit@users.noreply.github.com>
2022-06-29 18:44:59 -0400

    Add files via upload
    final upload

--- password.js
+++ password.js
@@ -11,10 +11,53 @@
 //Test out your conditional by changing the value of your password variable and running your file! */
 
 let password = "iLoveYou1!";
- let passwordLength = 10;
 
- if (password === passwordLength) {
- console.log('Password Successful - I love you 2!')
- } else {
-  console.log('You have faIled to find Love 1! =( Try Again')
- }
+//split string to array so we can loop over it
+let passwordSplit = password.split("");
+
+console.log(passwordSplit);
+
+let alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "x",
+  "y",
+  "z",
+];
+
+let nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+if (password.length >= 10) {
+  for (let i = 0; i < passwordSplit.length; i++) {
+    if (alphabet.includes(passwordSplit[i])) {
+      for (let j = i + 1; j < passwordSplit.length; j++) {
+        if (nums.includes(+passwordSplit[j])) {
+          return console.log(`password meets requirements`);
+        }
+      }
+    }
+    return console.log(`password doesnt meet requiremenets`);
+  }
+} else {
+  console.log(`did not meet length requirements,try again with 10 digits`);
+}
